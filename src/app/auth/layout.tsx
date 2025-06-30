3379ea1ce4e2a2e408eb3d7853b7baea0470b583
Millosaurs <millosaurs@gmail.com>
2025-06-30 15:21:10 +0000

    fix: build error

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,10 @@
+// /auth/layout.tsx
+import { Suspense } from "react";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>;
+}
