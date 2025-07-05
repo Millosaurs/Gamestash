3f4e6ec30356b27fdc1b96d4f7bf4b85838ec2e2
Millosaurs <millosaurs@gmail.com>
2025-07-05 07:53:47 +0000

    fix: build error v7

--- src/app/api/admin/categories/[id]/route.ts
+++ src/app/api/admin/categories/[id]/route.ts
@@ -3,10 +3,7 @@ import { games } from "@/db/schema";
 import { eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function DELETE(
-  request: Request,
-  context: { params: { id: string } }
-) {
+export async function DELETE(request: NextRequest, context: any) {
   const { id } = context.params;
   await db.delete(games).where(eq(games.id, Number(id)));
   return NextResponse.json({ success: true });
