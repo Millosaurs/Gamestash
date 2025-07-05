2c6e66a37f3aebaf359e2706c798c9d4b42d8f62
Millosaurs <millosaurs@gmail.com>
2025-07-05 07:51:00 +0000

    fix: build error v6

--- src/app/api/admin/categories/[id]/route.ts
+++ src/app/api/admin/categories/[id]/route.ts
@@ -1,26 +1,13 @@
 import { db } from "@/db";
-import { categories } from "@/db/schema";
+import { games } from "@/db/schema";
 import { eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function DELETE(
-  req: NextRequest,
+  request: Request,
   context: { params: { id: string } }
 ) {
   const { id } = context.params;
-  const numId = Number(id);
-
-  if (isNaN(numId)) {
-    return NextResponse.json({ error: "Invalid category ID" }, { status: 400 });
-  }
-
-  try {
-    await db.delete(categories).where(eq(categories.id, numId));
-    return NextResponse.json({ success: true });
-  } catch (error) {
-    return NextResponse.json(
-      { error: "Failed to delete category" },
-      { status: 500 }
-    );
-  }
+  await db.delete(games).where(eq(games.id, Number(id)));
+  return NextResponse.json({ success: true });
 }
